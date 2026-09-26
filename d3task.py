fb6264eb43e7df431b519a1caaf4a3a8caf5b8fd
bhuvana <bhuvanaaganesan07@gmail.com>
2026-09-26 14:08:43 +0530

    d3task.py created

---
+++ d3task.py
@@ -0,0 +1,10 @@
+name = Bhuvana
+college = PSNACET
+branch = IT
+year = 3
+print("=" *25)
+print(Name   :, name)
+print(College:, college)
+print(Branch :, branch)
+print(Year   :, year)
+print("=" *25)
